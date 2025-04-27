0ad4b0a5366bbe3407c280e3f492f8cd14a9cb9d
Srv_x13 <krsaurav1326@gmail.com>
2025-04-27 13:55:38 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Team Project to Analysis the Comment of YouTube using Machine Learning',
+    author='saurav',
+    license='',
+)
